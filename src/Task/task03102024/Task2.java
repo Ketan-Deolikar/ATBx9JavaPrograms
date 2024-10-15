c246f7adbdd62fb4dc2ed7e0c24e459fed05737e
Ketan <kdeolikar17@gmail.com>
2024-10-15 18:07:08 +0530

    My Files

---
+++ src/Task/task03102024/Task2.java
@@ -0,0 +1,13 @@
+package Task.task03102024;
+
+public class Task2 {
+    public static void main(String[] args) {
+        // Table a 10 - sop
+        System.out.println("10*1=10");
+        System.out.println("10*2=20");
+        System.out.println("10*3=30");
+        System.out.println("10*4=40");
+        System.out.println("...");
+        System.out.println("10*10=100");
+    }
+}
---
+++ src/Task/task03102024/Task3.java
@@ -0,0 +1,11 @@
+package Task.task03102024;
+
+public class Task3 {
+    public static void main(String[] args) {
+        //Calculator
+        System.out.println(10+3);
+        System.out.println(10-3);
+        System.out.println(10*3);
+        System.out.println(10/3);
+    }
+}

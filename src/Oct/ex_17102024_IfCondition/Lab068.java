8bf99c87f5af8215a0188766d5a42a877668e612
Ketan <kdeolikar17@gmail.com>
2024-10-21 11:55:09 +0530

    My Files 14 Oct

---
+++ src/Oct/ex_17102024_IfCondition/Lab068.java
@@ -0,0 +1,20 @@
+package Oct.ex_17102024_IfCondition;
+
+import java.util.Scanner;
+
+public class Lab068 {
+    public static void main(String[] args) {
+
+        //Scanner sc = new Scanner(System.in);
+        //System.out.println("Enter number");
+        //int a = sc.nextInt();
+
+        int a = 10;
+        if( a == 4){
+            System.out.println("Hahahah");
+        }
+        else {
+            System.out.println("Bye");
+        }
+    }
+}

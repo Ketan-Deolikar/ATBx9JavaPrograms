b0d05fdfb11fb52e374b51b90ae03ed69c996881
Ketan <kdeolikar17@gmail.com>
2024-10-24 17:23:38 +0530

    My Files 22 Oct

---
+++ src/Oct/ex_22102024_WhileLoop/Lab102.java
@@ -0,0 +1,16 @@
+package Oct.ex_22102024_WhileLoop;
+
+public class Lab102 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 10){
+            // Code
+            System.out.println(i);
+            i++;
+        }
+
+        for (int j = 0; j < 10; j++) {
+            System.out.println(j);
+        }
+    }
+}
---
+++ src/Oct/ex_22102024_WhileLoop/Lab103_While.java
@@ -0,0 +1,20 @@
+package Oct.ex_22102024_WhileLoop;
+
+public class Lab103_While {
+    public static void main(String[] args) {
+        int modi = 1;
+
+        float pi = 3.14f;
+        // 10,ooo lines
+        // Jr QA
+        pi = 5;
+        pi = 10;
+        System.out.println(pi);
+
+
+        while(modi <= 10) {
+            System.out.println("Modi 10 Years");
+            modi++;
+        }
+    }
+}

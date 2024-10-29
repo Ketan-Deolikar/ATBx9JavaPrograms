25b5437709509baec6e84820839db20d55435c88
Ketan <kdeolikar17@gmail.com>
2024-10-29 11:51:54 +0530

    My Files 24 Oct

---
+++ src/Oct/ex_24102024/Lab118.java
@@ -0,0 +1,13 @@
+package Oct.ex_24102024;
+
+import java.util.Scanner;
+
+public class Lab118 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the num1");
+        int a = sc.nextInt();
+
+    }
+}

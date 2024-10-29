74ed5e3d7df5461453343d9c0244d419662394fc
Ketan <kdeolikar17@gmail.com>
2024-10-29 11:22:29 +0530

    My Files 23 Oct

---
+++ src/Oct/ex_23102024_Functions/Lab111.java
@@ -0,0 +1,18 @@
+package Oct.ex_23102024_Functions;
+
+public class Lab111 {
+    public static void main(String[] args) {
+
+        greet();
+
+        // Code 10, 000
+
+        greet();
+
+        greet();
+    }
+
+    static void greet(){
+        System.out.println("Hi, How are you");
+    }
+}
---
+++ src/Oct/ex_23102024_Functions/Lab114.java
@@ -0,0 +1,80 @@
+package Oct.ex_23102024_Functions;
+
+public class Lab114 {
+    public static void main(String[] args) {
+
+        // User Defined Functions.
+
+        //  1.Without Parameters and Without Return Type
+        // 2.Without Parameters but With Return Type
+        // 3.With Parameters and Without Return Type
+        // 4. With Parameters and With Return Type
+
+
+
+
+        //
+        //  1. Without Argument / Parameters and Without Return Type.
+               for (int i = 0; i < 99; i++) {
+                    greet();
+               }
+
+        greet();
+
+        //  2. Without Parameters but With Return Type
+        String returned_value = greet_with_hello();
+        System.out.println(returned_value);
+
+        int voting_age = age_of_vote();
+        System.out.println(voting_age);
+        if (voting_age >= 18){
+            System.out.println("You sre allowed to vote!");
+        }else {
+            System.out.println("Not allowed!");
+        }
+
+        //  3. With Parameters and Without Return Type ( 90%)
+
+        greet_with_your_name("Ketan");
+        greet_with_your_name("Deolikar");
+
+        greet_with_first_name_last_name("Ketan", "Deolikar");
+
+        //  4. With Parameters and With Return Type
+        int result = sum_of_two_numbers(3,4);
+        System.out.println(result);
+
+    }
+
+    // 1. Without Parameters and Without Return Type. (Declare) /Define
+    static void greet(){
+        System.out.println("Hi, How are you bruhhh");
+    }
+
+    // 2. Without Parameters but With Return Type
+    static String greet_with_hello(){
+        // Write the code
+        System.out.println("Hi, I am type 2");
+        return "Hi, You are Awesome";
+    }
+
+    static int age_of_vote() {
+        // Write the code
+        System.out.println("Vote age returned!");
+        return 18;
+    }
+
+    //  3. With Parameters and Without Return Type (void)
+    static void greet_with_your_name(String name){
+        System.out.println("Hi, Your name is " + name);
+    }
+
+    static void greet_with_first_name_last_name(String firstname, String lastname){
+        System.out.println("Hi, Your name is " + firstname +" "+ lastname);
+    }
+
+    //  4. With Parameters and With Return Type
+    static int sum_of_two_numbers(int a, int b){
+        return a+b;
+    }
+}

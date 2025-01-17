346868c180c6290c21841fdc54ba401f02ed3547
Ketan <kdeolikar17@gmail.com>
2025-01-17 13:17:40 +0530

    Task 15th Oct

---
+++ src/Oct/MyPractice/BasicsOfJava/Operators/TernaryOperator/Lab001.java
@@ -0,0 +1,11 @@
+package Oct.MyPractice.BasicsOfJava.Operators.TernaryOperator;
+
+public class Lab001 {
+    public static void main(String[] args) {
+
+        int a = 10;
+
+        System.out.println(++a + a++ + a++);
+        System.out.println(a);
+    }
+}
---
+++ src/Oct/MyPractice/BasicsOfJava/Operators/TernaryOperator/Lab002.java
@@ -0,0 +1,9 @@
+package Oct.MyPractice.BasicsOfJava.Operators.TernaryOperator;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        int a = 20;
+        System.out.println(--a + a++ + a--);
+        System.out.println(a);
+    }
+}
